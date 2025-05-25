2267cc29e3e59de342323e950317e14143100a6b
Tombervilt <bad_hippie@mail.ru>
2025-05-25 04:26:59 +0300

    refactor int with double math operation added try infinity 'int' by math operation

--- src/main/java/tombervilt/a/MathOperations.java
+++ src/main/java/tombervilt/a/MathOperations.java
@@ -13,11 +13,8 @@ public class MathOperations {
         System.out.println("a * b = " + (a * b));
         System.out.println("a / b = " + (a / b));
 
-        System.out.println("\nАрифметические операции над int и double:\n");
-        System.out.println("a + c = " + (a + c));
-        System.out.println("a - d = " + (a - d));
-        System.out.println("b * c = " + (b * c));
-        System.out.println("b / c = " + (b / c));
+        System.out.println("\nАрифметические операции над int и double в одном выражении:\n");
+        System.out.println("(a + c) - d * c / c = " + ((a + c) - d * c / c));
 
         System.out.println("\nПрименение логических операций сравнения:\n");
         System.out.println("a > b - " + (a > b));
@@ -28,7 +25,10 @@ public class MathOperations {
         System.out.println("\nПереполнение при арефметических операциях:\n");
         float LimitFloat = Float.MAX_VALUE;;
         double LimitDouble = Double.MAX_VALUE;
+        int limitInt = Integer.MAX_VALUE;
         System.out.println("LimitFloat * 2 = " + (LimitFloat * 2));
         System.out.println("LimitDouble * 2 = " + (LimitDouble * 2));
+        System.out.println("limitInt * 2 = " + (limitInt * 2));
+        //Переполнение int невозможно, вместо этого происходит 'заворачивание' - переход от макс. значения 2,147,483,647 к мин. значения -2,147,483,648
     }
 }
